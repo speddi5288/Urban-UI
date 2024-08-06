5cc874ecf770c6bc939771fd271c67a9879a4cc3
Srisaketh Peddi <srisaketh.peddi@gmail.com>
2024-08-06 17:52:42 -0400

    New updates

--- form-process.php
+++ form-process.php
@@ -4,12 +4,21 @@
  *
  * @package Brome - Responsive Portfolio Creative Template
  */
-    $name=$_POST['name'];
-    $email=$_POST['email'];
-    $message=$_POST['message'];		
-	$from="From: $name<$email>\r\nReturn-path: $email";
-	$subject="Urban UI";
-	mail("urbanuiagency@gmail.com", $subject, $message, $from)
-?>
-   
 
+// Capture the form data
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+
+// Create the email headers
+$from = "From: $name<$email>\r\nReturn-path: $email";
+
+// Subject of the email
+$subject = "Urban UI";
+
+// Send the email
+mail("urbanuiagency@gmail.com", $subject, $message, $from);
+
+// Optional: Provide feedback to the user (e.g., a confirmation message)
+echo "Thank you for contacting us!";
+?>
